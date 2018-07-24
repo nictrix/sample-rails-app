effaf406bafea1e6ec564792a7c01631ab6c3c34
nictrix <nickwillever@gmail.com>
2018-07-23 17:57:06 -0700

    use headless chrome

--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_url
     assert_response :success
   end
